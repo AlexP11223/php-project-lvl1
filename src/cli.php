d5e8edbf826496315beb379c0f091f2de134e5a4
Alex P <alex.pantec@gmail.com>
2019-07-04 11:35:27 +0300

    bad commit

--- src/cli.php
+++ src/cli.php
@@ -6,7 +6,7 @@ use function \cli\line;
 
 function run()
 {
-    line('Welcome to the Brain Game!');
+ line('Welcome to the Brain Game!');
     $name = \cli\prompt('May I have your name?');
     line("Hello, %s!", $name);
 }
