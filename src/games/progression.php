254c9b1020708b459d0c46fc67406e2e66d36c91
Alex P <alex.pantec@gmail.com>
2019-07-23 12:31:28 +0300

    refactor

--- src/games/progression.php
+++ src/games/progression.php
@@ -2,20 +2,17 @@
 
 namespace BrainGames\games\progression;
 
-const PROGRESSION_LENGTH = 10;
+const PROGRESSION_ELEMENTS_COUNT = 10;
 const MIN_INCREMENT = -20;
 const MAX_INCREMENT = 20;
 const MIN_START_NUMBER = 0;
 const MAX_START_NUMBER = 100;
 
-function generateProgression()
+function generateProgression($start, $diff, $elementsCount)
 {
-    $start_num = rand(MIN_START_NUMBER, MAX_START_NUMBER);
-    $increment = rand(MIN_INCREMENT, MAX_INCREMENT);
-
     $result = [];
-    for ($i = 0; $i < PROGRESSION_LENGTH; $i++) {
-        $result[] = $start_num + $increment * $i;
+    for ($i = 0; $i < $elementsCount; $i++) {
+        $result[] = $start + $diff * $i;
     }
     return $result;
 }
@@ -25,7 +22,11 @@ function make()
     return [
         'description' => 'What number is missing in the progression?',
         'iteration' => function () {
-            $progression = generateProgression();
+            $progression = generateProgression(
+                rand(MIN_START_NUMBER, MAX_START_NUMBER),
+                rand(MIN_INCREMENT, MAX_INCREMENT),
+                PROGRESSION_ELEMENTS_COUNT
+            );
             $missingElementIndex = array_rand($progression);
             $missingElement = $progression[$missingElementIndex];
             $progression[$missingElementIndex] = '..';
